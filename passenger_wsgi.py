90eb8ebb7e341e4881accc19b9da36ea72fdb125
GschoesserPhilipp <philipp.gschoesser@gmail.com>
2025-07-29 09:48:20 +0200

    change wsgi file

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,8 @@
 import sys
 import os
 
+# Füge den src-Ordner dem Python-Importpfad hinzu
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
-from src.main import app as application
-
-if __name__ == "__main__":
-    application.run()
+# Importiere die Flask-App aus src/main.py
+from main import app as application
